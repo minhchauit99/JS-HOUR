07dd959d42c6df85176360e9b52e011fc8baa2d7
minhchauit99 <vominhchau01071999tv@gmail.com>
2022-04-30 23:35:38 +0700

    update

---
+++ ham.js
@@ -0,0 +1,19 @@
+//XEM LUỒNG CHẠY DỮ LIỆU TRONG JS 
+/*
+Function va Method
+*/
+// VI SAO PHAI CHIA HAM
+/*
+1. TỐI ƯU HÓA
+2. TÁI SỬ DỤNG CODE
+3, 
+*/
+//input parmare 
+//tham so dau vao 
+function sum(a, b) {
+    return a + b;
+}
+
+let c = sum(2, 10)
+console.log("sum a + b :", sum(4, 5))
+// console.log("sum c", c);
